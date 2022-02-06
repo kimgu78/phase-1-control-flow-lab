c3bd4ae41d32c54a0a2f9d857ec01c17e250f163
kimgu78 <kimgu78@gmail.com>
2022-02-05 17:02:29 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(aValue){
+  if (aValue <= 400) 
+  return `This one is on me!`; 
+  if (aValue > 2000 && aValue <=2500)
+  return `I will gladly take your thirty bucks.`;
+  else if (aValue > 2500)
+  return `No can do.`;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(aCity){
+  return aCity === `NYC` ?`Ok, sounds good.` :`No go.`;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(aTip){
+  switch (aTip) {
+    case `generous`: 
+    return `Thank you so much.`;
+    case `not as generous`: 
+    return `Thank you.`
+    default:
+    return `Bye.`
+  }
 }
